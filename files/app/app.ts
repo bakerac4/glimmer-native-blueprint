1c0bb56d8600e9a49792413859afc601de3b0426
Adam Baker <bakerac4@gmail.com>
2019-05-29 10:05:31 -0400

    Removing unused files

--- files/app/app.ts
+++ files/app/app.ts
@@ -6,8 +6,37 @@ const resolverDelegate = new ResolverDelegate();
 const resolver = new Resolver();
 let appFolder = knownFolders.currentApp();
 
+
+const addTemplates = (appFolder) => {
+  let templatesFile = appFolder.getFile('templates.json');
+  let templates = templatesFile.readTextSync();
+  // console.log(`Templates: ${templates}`);
+  JSON.parse(templates).forEach((template) => {
+    resolverDelegate.registerComponent(
+      template.name,
+      template.handle,
+      template.source,
+      template.capabilities
+    );
+  });
+};
+
+const addComponents = (appFolder) => {
+  let componentsFile = appFolder.getFile('components.json');
+  let components = componentsFile.readTextSync();
+  console.log(`About to resolve require`);
+  JSON.parse(components).forEach((component) => {
+    console.log(`About to resolve require`);
+    const classFile = require(`../src/ui/components/${component.name}/component.ts`);
+    resolver.registerComponent(component.name, classFile.default);
+  });
+};
+
+addTemplates(appFolder);
+addComponents(appFolder);
+
 const app = new Application('<%= component %>', resolverDelegate, resolver);
-app.setup(appFolder);
+// app.setup(appFolder);
 // const containerElement = document.getElementById('app');
 
 setPropertyDidChange(() => {
