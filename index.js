fc673a37286d4a0efbc006c1281a96d5dd5434d1
Adam Baker <bakerac4@gmail.com>
2019-06-05 08:03:39 -0400

    Remove unused afterinstall and modify gitignore to be like glimmer blueprint

--- index.js
+++ index.js
@@ -41,52 +41,7 @@ module.exports = {
     return files;
   },
 
-  afterInstall(options) {
-    if (options.webComponent) {
-      this._validateWebComponentName(options);
-      return this._installWebComponentSupport(options);
-    }
-  },
-
-  _validateWebComponentName(options) {
-    let customElementName = options.webComponent;
-
-    if(typeof customElementName !== 'string') {
-      throw 'You must provide a name for the web component, eg. `--web-component=button-list`';
-    }
-
-    if(!hasDash(customElementName)) {
-      throw `The web component name must contain a dash. You provided "${customElementName}"` ;
-    }
-  },
-
-  _installWebComponentSupport(options) {
-    let customElementName = options.webComponent;
-    let glimmerComponentName = classify(options.entity.name);
-
-    let addPackagePromise = this.addPackageToProject('@glimmer/web-component');
-    let indexTSPromise = this.insertIntoFile(
-      'src/index.ts',
-      "import initializeCustomElements from '@glimmer/web-component';",
-      { after: "import { setPropertyDidChange } from '@glimmer/tracking';\n" }
-    ).then(() => {
-      return this.insertIntoFile(
-        'src/index.ts',
-        `initializeCustomElements(app, { '${customElementName}': '${glimmerComponentName}' });\n`
-      );
-    });
-
-    return Promise.all([
-      addPackagePromise,
-      indexTSPromise
-    ]);
-  },
-
   _shouldIncludeYarnLockInFiles() {
     return !!this.project.pkg.name;
   }
 };
-
-function hasDash(string) {
-  return string.indexOf('-') > -1;
-}
